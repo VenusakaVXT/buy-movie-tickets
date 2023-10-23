4615f8177468fe17902b614193428ba903726f94
VenusakaVXT <voxuantuan33@gmail.com>
2023-10-23 14:04:41 +0700

    The PUT method updates the user (#6)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,12 @@
 import bcrypt from "bcrypt"
 import User from "../models/User.js"
 
+const salt = bcrypt.genSaltSync(10) // saltRounds: 10
+
+const hashUserPassword = (userPassword) => {
+    return bcrypt.hashSync(userPassword, salt)
+}
+
 export const getAllUsers = async (req, res, next) => {
     let users
 
@@ -23,8 +29,7 @@ export const register = async (req, res, next) => {
     const name = req.body.name
     const email = req.body.email
     const password = req.body.password
-    const salt = bcrypt.genSaltSync(10) // saltRounds: 10
-    const hashPassword = bcrypt.hashSync(password, salt)
+    const hashPassword = hashUserPassword(password)
 
     if (
         (!name || name.trim() === "") &&
@@ -52,4 +57,40 @@ export const register = async (req, res, next) => {
     }
 
     return res.status(201).json({ user })
+}
+
+export const updateUser = async (req, res, next) => {
+    const id = req.params.id
+    const { name, email, password } = req.body
+    const hashPassword = hashUserPassword(password)
+
+    if (
+        (!name || name.trim() === "") &&
+        (!email || email.trim() === "") &&
+        (!password || password.trim() === "")
+    ) {
+        return res.status(422).json({
+            message: "Invalid inputs..."
+        })
+    }
+
+    let user
+
+    try {
+        user = await User.findByIdAndUpdate(id, {
+            name,
+            email,
+            password: hashPassword
+        })
+    } catch(err) {
+        console.error(err)
+    }
+
+    if (!user) {
+        return res.status(500).json({
+            message: "Something went wrong..."
+        })
+    }
+
+    res.status(200).json({ message: "User update successfully!!!" })
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getAllUsers, register } from "../controllers/userController.js"
+import { getAllUsers, register, updateUser } from "../controllers/userController.js"
 
 const userRouter = express.Router()
 
 userRouter.get("/", getAllUsers)
 userRouter.post("/register", register)
+userRouter.put("/:id", updateUser)
 
 export default userRouter
