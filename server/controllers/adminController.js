aa5f6b521a91a694cb684972ad920a9f99c7c270
VenusakaVXT <voxuantuan33@gmail.com>
2023-10-23 16:14:30 +0700

    Generate token code when Admin logs in with JWT (#10)

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,5 +1,9 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 import Admin from "../models/Admin.js"
+
+dotenv.config()
 
 const salt = bcrypt.genSaltSync(10) // saltRounds: 10
 
@@ -71,5 +75,14 @@ export const adminLogin = async (req, res, next) => {
         return res.status(400).json({ message: "incorrect password..." })
     }
 
-    res.status(200).json({ message: "login successfully!!!" })
+    const token = jwt.sign({ id: existAdmin._id }, process.env.SECRET_KEY, {
+        expiresIn: "7d", // the token chain will expire after 7 days
+
+    })
+
+    res.status(200).json({ 
+        message: "login successfully!!!",
+        token,
+        id: existAdmin._id
+    })
 }
