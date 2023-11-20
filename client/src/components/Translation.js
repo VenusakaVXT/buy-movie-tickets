a71c6df0123d2f8dd4c188f6acd4e777e877237c
VenusakaVXT <voxuantuan33@gmail.com>
2023-11-20 20:30:27 +0700

    Write a demo of the language translation function for the entire website (#19)

---
+++ client/src/components/Translation.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react"
+import translate from "@google-cloud/translate"
+
+function Translation() {
+    const [language, setLanguage] = useState("US")
+    const [translatedContent, setTranslatedContent] = useState({})
+
+    useEffect(() => {
+        const detectLanguage = async () => {
+            const userLanguage = navigator.language || navigator.languages[0]
+            setLanguage(userLanguage)
+        }
+
+        detectLanguage()
+    }, [])
+
+    useEffect(() => {
+        const translateContent = async () => {
+            const translations = {}
+            const elementsToTranslate = document.querySelectorAll("[data-translate]")
+
+            for (const element of elementsToTranslate) {
+                const originalText = element.textContent
+                const translatedText = await translate(originalText, language)
+                translations[element.id] = translatedText
+            }
+
+            setTranslatedContent(translations)
+        }
+
+        translateContent()
+    }, [language])
+
+    const handleLanguageChange = async (newLanguage) => {
+        setLanguage(newLanguage)
+        const elementsToTranslate = document.querySelectorAll("[data-translate]")
+
+        for (const element of elementsToTranslate) {
+            const originalText = element.textContent
+            const translatedText = await translate(originalText, newLanguage)
+            element.textContent = translatedText
+        }
+    }
+
+    return {
+        language,
+        translatedContent,
+        handleLanguageChange,
+    }
+}
+
+// Excute
+/*
+    file ComponentName.js:
+    import Translation from "./components/Translation"
+
+    function ComponentName() {
+        const { language, translatedContent } = Translation()
+
+        <LanguageMenu handleLanguageChange={handleLanguageChange} />
+
+    <div>
+        {Object.entries(translatedContent).map(([id, translatedText]) => (
+            <div key={id} id={id} dangerouslySetInnerHTML={{ __html: translatedText }} />
+        ))}
+    </div>
+    }
+*/
+
+export default Translation
