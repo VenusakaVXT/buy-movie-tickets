c7db4e925fa07e64785ff5f629f57fca0b3eb6a1
VenusakaVXT <voxuantuan33@gmail.com>
2024-07-26 22:43:39 +0700

    Move the locales folder from public to src (#113)

--- client/src/util/i18n.js
+++ client/src/util/i18n.js
@@ -2,6 +2,8 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
 import Backend from "i18next-http-backend"
+import us from "../locales/us/translation.json"
+import vn from "../locales/vn/translation.json"
 
 i18n
     .use(Backend)
@@ -9,12 +11,17 @@ i18n
     .use(initReactI18next)
     .init({
         debug: true,
-        fallbackLng: "en",
-        backend: {
-            loadPath: "/locales/{{lng}}/{{cn}}.json"
-        },
+        fallbackLng: "us",
         interpolation: {
             escapeValue: false,
+        },
+        resources: {
+            us: {
+                translation: us
+            },
+            vn: {
+                translation: vn
+            }
         }
     })
 
